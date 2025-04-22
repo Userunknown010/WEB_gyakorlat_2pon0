83c11fdeca4e2215476336e96d19fefc2d7c2648
ShadowDev <gacov.richard0630@gmail.com>
2025-04-22 14:38:10 +0200

    végleges css, befejezés

--- templates/pages/kilepes.tpl.php
+++ templates/pages/kilepes.tpl.php
@@ -1,2 +1,6 @@
-<h1>Kilépett:</h1>
-<?= $data['csn']." ".$data['un']." (".$data['login'].")" ?>
+<div class="kilepcontainer">
+    <div class="card shadow-sm p-4">
+        <h1>Kilépett:</h1>
+        <?= $data['csn']." ".$data['un']." (".$data['login'].")" ?>  
+    </div>
+</div>
--- templates/pages/regisztral.tpl.php
+++ templates/pages/regisztral.tpl.php
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Regisztráció</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
+<div class="regisztralcontainer">
+    <div class="card shadow-sm p-4">
         <?php if(isset($uzenet)) { ?>
             <h1><?= $uzenet ?></h1>
             <?php if($ujra) { ?>
-                <a href="index.php?oldal=belepes">Próbálja újra!</a>
+                <a id="ujra" href="index.php?oldal=belepes">Próbálja újra!</a>
             <?php } ?>
         <?php } ?>
-    </body>  
-</html>
+    </div>   
+</div>
